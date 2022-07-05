22fa7ab8b9d0a19f27ec6db79a02372ee4195538
Leehyoju97 <dlgywn2420@gmail.com>
2022-07-05 17:42:56 +0900

    두번째 버전

---
+++ src/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputData() {
+        return scanner.nextLine();
+    }
+}
